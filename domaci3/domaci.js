46ea29812787ae4df462ce5869e4d9a4fa68a1ee
markovic38164 <markovic38164@gmail.com>
2026-03-20 13:59:53 +0100

    domaci3

---
+++ domaci3/domaci.js
@@ -0,0 +1,10 @@
+let name = 'admin';
+let password = 'tajnaSifra';
+
+if (name === 'admin' && password === 'tajnaSifra') {
+	console.log('Dobrodosli nazad!');
+} else if (name === 'toma' && password === 'tajnaSifra') {
+	console.log('Dobrodošao Tomo!');
+} else {
+	console.log('Pogrešna lozinka ili ime!');
+}
