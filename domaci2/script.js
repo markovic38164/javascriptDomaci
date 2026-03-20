1f0119c8420a1a1de45919cb562bb58ba3d49a05
markovic38164 <markovic38164@gmail.com>
2026-03-20 13:04:57 +0100

    domaci2

---
+++ domaci2/script.js
@@ -0,0 +1,18 @@
+let shop = {
+	name: "Stefan's Shop",
+	products: [
+		'Mouse',
+		'Keyboard',
+		'HDD',
+		'RAM',
+		'Monitor',
+		'CPU',
+		'GPU',
+		'Motherboard',
+		'SSD',
+		'Power Supply',
+	],
+	openingHours: 8,
+	closingHours: 17,
+};
+console.log(shop);
