a16adc86c27b171df50443f209b94ad1da4550a7
markovic38164 <markovic38164@gmail.com>
2026-03-21 18:31:15 +0100

    domaci4

---
+++ domaci4/domaci.js
@@ -0,0 +1,13 @@
+let dayOfTheWeek = 'subota';
+
+switch (dayOfTheWeek) {
+	case 'nedelja':
+	case 'subota':
+		console.log('Weekend!');
+		break;
+	case 'petak':
+		console.log('Weekend is coming');
+		break;
+	default:
+		console.log('Work work!');
+}
