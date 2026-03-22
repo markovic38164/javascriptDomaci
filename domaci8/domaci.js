c9bd4dabde388c1728099c052db82afaf2e25a5f
markovic38164 <markovic38164@gmail.com>
2026-03-22 14:15:52 +0100

    domaci8

---
+++ domaci8/domaci.js
@@ -0,0 +1,248 @@
+let cars = [
+	{
+		name: 'Audi A4',
+		price: 9999,
+		year: 2026,
+		image:
+			'https://images.unsplash.com/photo-1606152421802-db97b9c7a11b?q=80&w=600',
+	},
+	{
+		name: 'BMW 3 Series',
+		price: 12000,
+		year: 2020,
+		image:
+			'https://images.unsplash.com/photo-1555215695-3004980ad54e?q=80&w=600',
+	},
+	{
+		name: 'Mercedes C-Class',
+		price: 15000,
+		year: 2022,
+		image:
+			'https://images.unsplash.com/photo-1618843479313-40f8afb4b4d8?q=80&w=600',
+	},
+	{
+		name: 'Volkswagen Golf 7',
+		price: 8500,
+		year: 2017,
+		image:
+			'https://images.unsplash.com/photo-1541899481282-d53bffe3c35d?q=80&w=600',
+	},
+	{
+		name: 'Skoda Octavia',
+		price: 4800,
+		year: 2026,
+		image: 'https://www.topgear.com/sites/default/files/2024/10/DSC02736.jpg',
+	},
+	{
+		name: 'Ford Focus',
+		price: 7000,
+		year: 2019,
+		image:
+			'https://media.drive.com.au/obj/tx_q:70,rs:auto:1920:1080:1/caradvice/private/1078812db2d5b4d5d3c78f1a3ae4b855',
+	},
+	{
+		name: 'Opel Astra',
+		price: 4200,
+		year: 2026,
+		image: 'https://cdn.motor1.com/images/mgl/bq6yk/s1/opel-astra-2021.webp',
+	},
+	{
+		name: 'Peugeot 308',
+		price: 11000,
+		year: 2021,
+		image:
+			'https://gcdn.polovniautomobili.com/user-images/thumbs/2821/28219463/791412bd58e6-800x600.jpg',
+	},
+	{
+		name: 'Audi A4',
+		price: 9999,
+		year: 2026,
+		image:
+			'https://images.unsplash.com/photo-1606152421802-db97b9c7a11b?q=80&w=600',
+	},
+	{
+		name: 'BMW 3 Series',
+		price: 12000,
+		year: 2020,
+		image:
+			'https://images.unsplash.com/photo-1555215695-3004980ad54e?q=80&w=600',
+	},
+	{
+		name: 'Mercedes C-Class',
+		price: 15000,
+		year: 2022,
+		image:
+			'https://images.unsplash.com/photo-1618843479313-40f8afb4b4d8?q=80&w=600',
+	},
+	{
+		name: 'Volkswagen Golf 7',
+		price: 8500,
+		year: 2017,
+		image:
+			'https://images.unsplash.com/photo-1541899481282-d53bffe3c35d?q=80&w=600',
+	},
+	{
+		name: 'Skoda Octavia',
+		price: 4800,
+		year: 2026,
+		image: 'https://www.topgear.com/sites/default/files/2024/10/DSC02736.jpg',
+	},
+	{
+		name: 'Ford Focus',
+		price: 7000,
+		year: 2019,
+		image:
+			'https://media.drive.com.au/obj/tx_q:70,rs:auto:1920:1080:1/caradvice/private/1078812db2d5b4d5d3c78f1a3ae4b855',
+	},
+	{
+		name: 'Opel Astra',
+		price: 4200,
+		year: 2026,
+		image: 'https://cdn.motor1.com/images/mgl/bq6yk/s1/opel-astra-2021.webp',
+	},
+	{
+		name: 'Peugeot 308',
+		price: 11000,
+		year: 2021,
+		image:
+			'https://gcdn.polovniautomobili.com/user-images/thumbs/2821/28219463/791412bd58e6-800x600.jpg',
+	},
+	{
+		name: 'Audi A4',
+		price: 9999,
+		year: 2026,
+		image:
+			'https://images.unsplash.com/photo-1606152421802-db97b9c7a11b?q=80&w=600',
+	},
+	{
+		name: 'BMW 3 Series',
+		price: 12000,
+		year: 2020,
+		image:
+			'https://images.unsplash.com/photo-1555215695-3004980ad54e?q=80&w=600',
+	},
+	{
+		name: 'Mercedes C-Class',
+		price: 15000,
+		year: 2022,
+		image:
+			'https://images.unsplash.com/photo-1618843479313-40f8afb4b4d8?q=80&w=600',
+	},
+	{
+		name: 'Volkswagen Golf 7',
+		price: 8500,
+		year: 2017,
+		image:
+			'https://images.unsplash.com/photo-1541899481282-d53bffe3c35d?q=80&w=600',
+	},
+	{
+		name: 'Skoda Octavia',
+		price: 4800,
+		year: 2026,
+		image: 'https://www.topgear.com/sites/default/files/2024/10/DSC02736.jpg',
+	},
+	{
+		name: 'Ford Focus',
+		price: 7000,
+		year: 2019,
+		image:
+			'https://media.drive.com.au/obj/tx_q:70,rs:auto:1920:1080:1/caradvice/private/1078812db2d5b4d5d3c78f1a3ae4b855',
+	},
+	{
+		name: 'Opel Astra',
+		price: 4200,
+		year: 2026,
+		image: 'https://cdn.motor1.com/images/mgl/bq6yk/s1/opel-astra-2021.webp',
+	},
+	{
+		name: 'Peugeot 308',
+		price: 11000,
+		year: 2021,
+		image:
+			'https://gcdn.polovniautomobili.com/user-images/thumbs/2821/28219463/791412bd58e6-800x600.jpg',
+	},
+	{
+		name: 'Audi A4',
+		price: 9999,
+		year: 2026,
+		image:
+			'https://images.unsplash.com/photo-1606152421802-db97b9c7a11b?q=80&w=600',
+	},
+	{
+		name: 'BMW 3 Series',
+		price: 12000,
+		year: 2020,
+		image:
+			'https://images.unsplash.com/photo-1555215695-3004980ad54e?q=80&w=600',
+	},
+	{
+		name: 'Mercedes C-Class',
+		price: 15000,
+		year: 2022,
+		image:
+			'https://images.unsplash.com/photo-1618843479313-40f8afb4b4d8?q=80&w=600',
+	},
+	{
+		name: 'Volkswagen Golf 7',
+		price: 8500,
+		year: 2017,
+		image:
+			'https://images.unsplash.com/photo-1541899481282-d53bffe3c35d?q=80&w=600',
+	},
+	{
+		name: 'Skoda Octavia',
+		price: 4800,
+		year: 2026,
+		image: 'https://www.topgear.com/sites/default/files/2024/10/DSC02736.jpg',
+	},
+	{
+		name: 'Ford Focus',
+		price: 7000,
+		year: 2019,
+		image:
+			'https://media.drive.com.au/obj/tx_q:70,rs:auto:1920:1080:1/caradvice/private/1078812db2d5b4d5d3c78f1a3ae4b855',
+	},
+	{
+		name: 'Opel Astra',
+		price: 4200,
+		year: 2026,
+		image: 'https://cdn.motor1.com/images/mgl/bq6yk/s1/opel-astra-2021.webp',
+	},
+	{
+		name: 'Peugeot 308',
+		price: 11000,
+		year: 2021,
+		image:
+			'https://gcdn.polovniautomobili.com/user-images/thumbs/2821/28219463/791412bd58e6-800x600.jpg',
+	},
+];
+
+let carsDiv = document.querySelector('.cars');
+for (let car of cars) {
+	let divEl = document.createElement('div');
+	divEl.classList.add('car');
+	let spanNovo = document.createElement('span');
+	if (car.year === 2026) {
+		spanNovo.classList.add('novo');
+	} else {
+		spanNovo.classList.add('hidden');
+	}
+	let imgCar = document.createElement('img');
+	let naslov = document.createElement('h2');
+	let divInfo = document.createElement('div');
+	divInfo.classList.add('info');
+	let spanCena = document.createElement('span');
+	spanCena.classList.add('cena');
+	let spanGodinaProizvodnje = document.createElement('span');
+	spanGodinaProizvodnje.classList.add('godinaProizvodnje');
+
+	spanNovo.textContent = 'NOVO';
+	imgCar.src = car.image;
+	imgCar.alt = 'car image';
+	naslov.textContent = car.name;
+	spanCena.textContent = car.price + ' €';
+	spanGodinaProizvodnje.textContent = car.year + '. god';
+	divInfo.append(spanCena, spanGodinaProizvodnje);
+	divEl.append(spanNovo, imgCar, naslov, divInfo);
+	carsDiv.append(divEl);
+}
