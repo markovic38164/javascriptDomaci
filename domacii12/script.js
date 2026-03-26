f02a94517100ff7efe92dc18fb981dc4b9779026
markovic38164 <markovic38164@gmail.com>
2026-03-26 13:52:45 +0100

    domacii12

---
+++ domacii12/script.js
@@ -0,0 +1,8 @@
+function login(username, lozinka) {
+	if (username.toLowerCase() === 'admin' && lozinka.toLowerCase() === 'admin') {
+		return true;
+	}
+	return false;
+}
+
+console.log(login('Admin', 'admin'));
